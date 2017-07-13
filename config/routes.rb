b0b72e17e3bc27933e7c0e44268637c343a3436b
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 17:49:51 +0000

    Added published button

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'Lol', to: 'pages#contact'
 
-  resources :blogs
+ root to: 'pages#home'
+ resources :blogs do
+    member do
+      get :toggle_status
+ end
+ end 
   
-  root to: 'pages#home'
-
 end
